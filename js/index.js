106a45506ef0cd3079ab0a2b3befd2ecc872a6b6
mainstory <108094683+mainstory@users.noreply.github.com>
2022-07-08 17:59:24 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,32 +1,39 @@
-const headerLinks = document.querySelectorAll('.menu__link')
-const sections = document.querySelectorAll('.section')
+function scrollBacklight() {
+   const headerLinks = document.querySelectorAll('.menu__link')
+   const sections = document.querySelectorAll('.section')
 
-document.addEventListener('scroll', () => {
+   if (headerLinks.length > 0) {
+      if (sections.length > 0) {
+         document.addEventListener('scroll', () => {
 
-   sections.forEach((section) => {
-      const top = section.getBoundingClientRect().top
+            sections.forEach((section) => {
+               const top = section.getBoundingClientRect().top
 
-      if (top <= 0) {
+               if (top <= 0) {
 
-         const attribut = section.getAttribute('data-name')
-         headerLinks.forEach((headerLink) => {
-            const id = headerLink.getAttribute('id')
-            if (id === attribut) {
-               headerLink.classList.add('_active')
-            } else {
-               headerLink.classList.remove('_active')
-            }
+                  const attribut = section.getAttribute('data-name')
+                  headerLinks.forEach((headerLink) => {
+                     const id = headerLink.getAttribute('id')
+                     if (id === attribut) {
+                        headerLink.classList.add('_active')
+                     } else {
+                        headerLink.classList.remove('_active')
+                     }
+
+
+                  })
+
+               }
+            })
 
 
          })
-
       }
-   })
 
+   }
+}
 
-})
-
-
+scrollBacklight()
 
 
 
